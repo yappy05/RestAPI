9e2bfe584f6772fa0ecd40200761129e2261a1fd
sergey <wendingnn@gmail.com>
2025-04-12 21:51:07 +0300

    init repo

---
+++ src/main/java/ReastAPI/example/ReastAPI/ReastApiApplication.java
@@ -0,0 +1,13 @@
+package ReastAPI.example.ReastAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReastApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ReastAPI/example/ReastAPI/ReastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ReastAPI.example.ReastAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
